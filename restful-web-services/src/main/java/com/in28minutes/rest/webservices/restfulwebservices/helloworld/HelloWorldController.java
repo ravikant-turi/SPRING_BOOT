1054b79fa2b3c53406034341e2d0bf39d4e00f0b
ravikant-turi <turiravikant@gmail.com>
2024-05-20 09:51:03 +0530

    first commit
    buildiing a restful wev service

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,8 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+}
